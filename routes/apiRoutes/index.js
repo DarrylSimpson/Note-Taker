e04885a59a7064137c02ab1f104111c694ed57e0
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-11-15 13:05:20 -0600

    used router to rout html and api files

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const createNote = require('../../lib/notes.js');
+let notes = require('../../db/db.json');
+
+
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+});
+
+router.post('/notes', (req, res) => {
+    const note = createNote(req.body, notes);
+    res.json(note);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require('fs');
-const path = require('path');
-let notesArray = require('./db/db.json');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.post('/api/notes', (req, res) => {
-    const note = createNote(req.body, notes);
-    res.json(note);
-});
 
 app.listen(PORT, () => {
-    console.log(`Server not on port ${PORT}`);
+    console.log(`Server now is on port ${PORT}`);
 });
